e47969cd936097823b908863f9d04741d8597919
psychotech27 <78391974+psycho2716@users.noreply.github.com>
2024-12-15 09:47:39 +0800

    Production Ready

--- components/navigation/navbar.tsx
+++ components/navigation/navbar.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import React from "react";
 import { fetchUser } from "@/actions/user-actions";
 import NavbarLinks from "./navbar-links";
+import Image from "next/image";
 
 const Navbar = async () => {
     const user = await fetchUser();
@@ -9,7 +10,11 @@ const Navbar = async () => {
     return (
         <header className="fixed top-0 left-0 right-0 z-50 bg-background/90 backdrop-blur-lg border-b border-border/40">
             <div className="container mx-auto px-4 sm:px-8 h-16 flex justify-between items-center">
-                <Link href="/" className="text-2xl font-bold text-[#20c997]">
+                <Link
+                    href="/"
+                    className="flex items-center gap-1 text-2xl font-bold text-[#20c997]"
+                >
+                    <Image src="/logo.png" alt="ZenSpace" width={40} height={40} className="ml-2" />
                     ZenSpace
                 </Link>
 
